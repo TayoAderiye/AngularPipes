27405864d299cbcd0018c9bbb1cf5e3f486ea8ee
TayoAderiye <tayoriye@gmail.com>
2021-08-18 20:57:10 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  name!: string
+  date!: string
+  amount!: number
+  height!: number
+  miles!: number
+
+  car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2000
+  }
+
+  onNameChange(event: any){
+    const target = event.target as HTMLInputElement;
+    this.name = target.value
+  }
+
+  onDateChange(event: any){
+    const target = event.target as HTMLInputElement;
+    this.date = target.value
+  }
+
+  onAmountChange(event: any){
+    const target = event.target as HTMLInputElement;
+    this.amount = parseFloat(target.value)
+  }
+
+  onHeightChange(event: any){
+    const target = event.target as HTMLInputElement
+    this.height = parseFloat(target.value)
+  }
+
+  onMilesChange(event: any){
+    const target = event.target as HTMLInputElement
+    this.miles = parseFloat(target.value)
+  }
 }
